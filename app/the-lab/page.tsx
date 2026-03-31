2cc1d46414589102a0296626c8a9821e1ad4041f
xenon <demiladeafuape74@gmail.com>
2026-03-31 22:30:43 +0100

    fixed import err 2

--- app/the-lab/page.tsx
+++ app/the-lab/page.tsx
@@ -1,17 +1,18 @@
 // import Hero from "./components/layout/Hero";
 // import Marquee from "./components/home/Marquee";
-import FeaturedGrid from "./components/home/FeaturedGrid";
+// import FeaturedGrid from "./components/home/FeaturedGrid";
 import EditorialBento from "./components/home/EditorialBento";
 import Newsletter from "./components/home/Newsletter";
 import Hero from "../components/layout/Hero";
 import Marquee from "../components/home/Marquee";
+import FeaturedDrop from "../components/home/FeaturedDrop";
 
 export default function Home() {
   return (
     <main className="bg-brand-black text-white pt-16">
       <Hero />
       <Marquee />
-      <FeaturedGrid />
+      <FeaturedDrop/>
       <EditorialBento />
       <Newsletter />
     </main>
