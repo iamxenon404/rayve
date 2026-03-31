3f3c07c5af911749bf4493dafb56c986f351c650
xenon <demiladeafuape74@gmail.com>
2026-03-31 22:30:57 +0100

    fixed import err 3

--- app/the-lab/page.tsx
+++ app/the-lab/page.tsx
@@ -1,11 +1,12 @@
 // import Hero from "./components/layout/Hero";
 // import Marquee from "./components/home/Marquee";
 // import FeaturedGrid from "./components/home/FeaturedGrid";
-import EditorialBento from "./components/home/EditorialBento";
+// import EditorialBento from "./components/home/EditorialBento";
 import Newsletter from "./components/home/Newsletter";
 import Hero from "../components/layout/Hero";
 import Marquee from "../components/home/Marquee";
 import FeaturedDrop from "../components/home/FeaturedDrop";
+import EditorialBento from "../components/home/EditorialBento";
 
 export default function Home() {
   return (
