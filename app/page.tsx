58bcbc7f10dc6da21cd34f9ae0932e1f5d12079c
xenon <demiladeafuape74@gmail.com>
2026-04-30 22:09:08 +0100

    updated root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,28 @@
-// import Hero from "./components/layout/Hero";
-// import Marquee from "./components/home/Marquee";
-// import FeaturedGrid from "./components/home/FeaturedGrid";
-// import EditorialBento from "./components/home/EditorialBento";
-// import Newsletter from "./components/home/Newsletter";
-// import Hero from "../components/layout/Hero";
-// import Marquee from "../components/home/Marquee";
-// import FeaturedDrop from "../components/home/FeaturedDrop";
-// import EditorialBento from "../components/home/EditorialBento";
-// import NewsletterModal from "../components/ui/NewsletterModal";
-
-import EditorialBento from "./components/home/EditorialBento";
-import FeaturedDrop from "./components/home/FeaturedDrop";
-import Marquee from "./components/home/Marquee";
 import Hero from "./components/layout/Hero";
+import Marquee from "./components/home/Marquee";
+import FeaturedDrop from "./components/home/FeaturedDrop";
+import EditorialBento from "./components/home/EditorialBento";
+// import Newsletter from "./components/home/Newsletter";
 
 export default function Home() {
   return (
     <main className="bg-brand-black text-white pt-16">
+      {/* Visual Identity & Video */}
       <Hero />
+      
+      {/* Dynamic Movement bar */}
       <Marquee />
-      <FeaturedDrop/>
+      
+      {/* Featured Products Grid */}
+      <div className="py-10">
+        <FeaturedDrop />
+      </div>
+      
+      {/* Brand Narrative / Bento Grid */}
       <EditorialBento />
-      {/* <NewsletterModal /> */}
+      
+      {/* Final Call to Action */}
+      {/* <Newsletter /> */}
     </main>
   );
 }
